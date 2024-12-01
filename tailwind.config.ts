310cd3bee149c7e9f0cd26cb35cda60ce5e13dfd
Ryosuke-Fukuchi <ryo12su29ke@gmail.com>
2024-12-01 20:22:55 +0900

    tailwindcss: border-3の追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      borderWidth: {
+        "3": "3px",
+      },
     },
   },
   plugins: [],
