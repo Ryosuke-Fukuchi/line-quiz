d8b4291bfbef551301a2c8ebd58fdf02bc7481bb
Ryosuke-Fukuchi <ryo12su29ke@gmail.com>
2024-12-04 23:27:17 +0900

    QuestionSortContent: answerの作成処理

--- src/app/question/[id]/page.tsx
+++ src/app/question/[id]/page.tsx
@@ -34,7 +34,7 @@ export default async function QuestionContentPage({
     <main className="min-h-screen px-8 py-4 pb-20 flex flex-col">
       <div className="p-1">
         <h3 className="text-lg font-semibold text-neutral-700 text-center">
-          第1問!
+          第{question.question_number}問!
         </h3>
       </div>
       <QuestionContent
--- src/features/question/[id]/QuestionSortContent.tsx
+++ src/features/question/[id]/QuestionSortContent.tsx
@@ -1,9 +1,14 @@
 "use client";
 import React from "react";
 import { QuestionSortType } from "@/requests/question";
-import { PlayerType } from "@/requests/player";
+import { createPlayerAnswer, patchPlayer, PlayerType } from "@/requests/player";
+import { useRouter } from "next/navigation";
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+import { QUESTION_TYPE } from "@/const.ts/question";
+import { PLAYER_STATUS } from "@/const.ts/player";
 
-// type ChoiceType = { pk: number; value: string; is_answer: boolean };
+type ChoiceType = { pk: number; value: string; correct_sort_order: number };
 
 type PropsType = {
   questionId: number;
@@ -13,6 +18,120 @@ type PropsType = {
   isLastQuestion: boolean;
 };
 
-export const QuestionSortContent: React.FC<PropsType> = () => {
-  return <div className="p-4 grow flex flex-col justify-between"></div>;
+export const QuestionSortContent: React.FC<PropsType> = ({
+  questionId,
+  questionNumber,
+  question,
+  player,
+  isLastQuestion,
+}) => {
+  const choices = React.useMemo(
+    () =>
+      question.questionsortchoice_set
+        .map((choice) => ({ ...choice, pk: choice.id, value: choice.content }))
+        .sort((a, b) => a.display_sort_order - b.display_sort_order),
+    [question]
+  );
+
+  const [selected, setClicked] = React.useState<ChoiceType[]>([]);
+  const router = useRouter();
+
+  const selectChoice = (selected: ChoiceType) => {
+    setClicked((current) => {
+      if (selected.pk === current[current.length - 1]?.pk) {
+        return current.slice(0, -1);
+      }
+
+      if (current.find((item) => selected.pk === item.pk)) {
+        return current;
+      }
+
+      return [...current, selected];
+    });
+  };
+
+  const [loading, setLoading] = React.useState(false);
+
+  const handleAnswer = async () => {
+    setLoading(true);
+    const selectedValues = selected.map((item) => item.value);
+    // 正解数
+    const correctCounts = selected.filter(
+      (item, i) => item.correct_sort_order === i + 1
+    ).length;
+    // 全問正解かどうか
+    const isPerfect = correctCounts === choices.length;
+    // 獲得ポイント
+    const earnedPoints =
+      question.answer_points * correctCounts +
+      (isPerfect ? question.additional_points : 0);
+
+    const answerPayload = {
+      content: JSON.stringify(selectedValues),
+      question_type: QUESTION_TYPE.sort,
+      earned_points: earnedPoints,
+      question_number: questionNumber,
+      question_id: questionId,
+    };
+
+    const playerPayload = {
+      id: player.id,
+      earned_points: player.earned_points + earnedPoints,
+      question_number: questionNumber + 1,
+      status: isLastQuestion ? PLAYER_STATUS.done : PLAYER_STATUS.playing,
+    };
+
+    await createPlayerAnswer(player.id, answerPayload);
+    await patchPlayer(player.id, playerPayload);
+
+    // /questionに遷移
+    router.push("/question");
+    setLoading(false);
+  };
+
+  return (
+    <div className="p-4 grow flex flex-col justify-between items-center">
+      <div className="p-4 grid grid-cols-3 gap-3 w-fit">
+        {choices.map((choice) => (
+          <div key={choice.pk.toString()} className="relative">
+            {selected.findIndex((item) => item.pk === choice.pk) >= 0 && (
+              <div className="absolute text-sm font-semibold size-8 border-2 border-teal-900 bg-white -top-2 -left-2 rounded-full flex justify-center items-center">
+                {selected.findIndex((item) => item.pk === choice.pk) + 1}
+              </div>
+            )}
+            <button
+              type="button"
+              className={twMerge(
+                "text-lg size-28 box-border border border-teal-600 bg-green-50 rounded-md p-2",
+                clsx(
+                  selected.find((item) => choice.pk === item.pk) &&
+                    "border-3 bg-green-100 font-semibold"
+                )
+              )}
+              onClick={() => {
+                selectChoice(choice);
+              }}
+            >
+              {choice.value}
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center py-6">
+        <button
+          className={twMerge(
+            "border-2 border-zinc-300 bg-zinc-200 text-zinc-400 rounded text-lg tracking-wide py-2 px-6",
+            clsx(
+              selected.length === choices.length &&
+                "font-semibold border-emerald-700 text-white bg-emerald-700 hover:border-emerald-700 hover:bg-white hover:text-emerald-700 shadow-md shadow-emerald-900/40 active:shadow-none"
+            )
+          )}
+          disabled={selected.length !== choices.length || loading}
+          onClick={handleAnswer}
+        >
+          {loading ? "loading..." : "回答する"}
+        </button>
+      </div>
+    </div>
+  );
 };
