95975b0dabb9555f0c0023b45f8a575a349780b8
Ryosuke-Fukuchi <ryo12su29ke@gmail.com>
2025-02-16 00:08:27 +0900

    style: refactor button styling in JoinSuccessView component

--- src/app/(quiz)/quiz/[id]/SuccessView.tsx
+++ src/app/(quiz)/quiz/[id]/SuccessView.tsx
@@ -3,6 +3,9 @@
 import { PlayerType } from "@/types/playerTypes";
 import Link from "next/link";
 
+const buttonStyle =
+  "border-2 border-emerald-700 text-white rounded-md bg-emerald-700 hover:border-emerald-700 hover:bg-white hover:text-emerald-700 font-semibold text-xl tracking-wide shadow-md shadow-emerald-900/40 active:shadow-none py-2 px-6";
+
 export const JoinSuccessView: React.FC<{
   player: PlayerType;
 }> = ({ player }) => {
@@ -22,12 +25,7 @@ export const JoinSuccessView: React.FC<{
       </div>
       <div className="grow flex items-end py-6">
         <Link href={`/question/${player.next_question_id}`}>
-          <button
-            className="border-2 border-emerald-700 text-white rounded bg-emerald-700 hover:border-emerald-700 hover:bg-white 
-            hover:text-emerald-700 font-semibold text-lg tracking-wide shadow-md shadow-emerald-900/40 active:shadow-none py-2 px-6"
-          >
-            第1問に進む
-          </button>
+          <button className={buttonStyle}>第1問に進む</button>
         </Link>
       </div>
     </main>
